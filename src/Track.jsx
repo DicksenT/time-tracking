946dcaa7c5d217c60a2667699d645ee6fa60fdce
Dicksen Tan <tandicksen@gmail.com>
2024-02-19 08:18:12 +0700

    Implement Counting function on Track Component

--- src/Track.jsx
+++ src/Track.jsx
@@ -1,8 +1,29 @@
 
+import { useEffect, useState } from 'react'
 import ellipsis from './images/icon-ellipsis.svg'
 
 function Track(props){
-    
+    function count(status){
+    const [numUp, setNumUp] = useState(0)
+    useEffect(() =>{
+        setNumUp(0);
+        let duration = 500 / status
+        let counter = setInterval(()=>{
+            setNumUp(prevNumUp => {
+                if (prevNumUp < status) {
+                  return prevNumUp + 1;
+                } else {
+                  clearInterval(counter);
+                  return prevNumUp;
+                }
+              });
+              
+        },duration)
+        return ()=> {clearInterval(counter)}        
+    },[status])
+    return <span>{numUp}</span>
+}
+
     const bgColor = {
         'backgroundColor': props.bgColor
     }
@@ -11,11 +32,12 @@ function Track(props){
         'daily': 'day',
         'weekly': 'week',
         'monthly': 'month'
+        
     }
 
     return(
-        <li className="track">
-            <div className="top-img-container" style={bgColor}>
+        <li className="track" style={bgColor}>
+            <div className="top-img-container" >
                 <img src={props.img} alt="" />
             </div>
             <div className="info">
@@ -29,16 +51,16 @@ function Track(props){
                 </div>
                 <div className="time">
                     <span className="now">
-                        {props.now}hrs
+                        {count(props.now)}hrs
                     </span>
                     <p>
-                        This {interval[props.interval]} - {props.last}
+                        this {interval[props.interval]} - {count(props.last)}hrs
                     </p>
                 </div>
-
-
             </div>
         </li>
     )
+
 }
+
 export default Track
