c1f3e3e37986cb41d4c89bb13a73657072455bd5
Dicksen Tan <tandicksen@gmail.com>
2024-02-21 19:35:42 +0700

    update:  json address

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   useEffect(() =>{
     async function getData(){
       try{
-        const response = await fetch('./public/data.json')
+        const response = await fetch('./data.json')
         if(!response.ok){
           console.log(response.status)
           return
